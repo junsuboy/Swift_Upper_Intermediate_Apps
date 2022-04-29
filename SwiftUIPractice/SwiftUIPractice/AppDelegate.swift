50e965d3879cc76a6bed51752ee1a056b358898f
Ik Jum Moon <86935775+junsuboy@users.noreply.github.com>
2022-04-30 07:56:12 +0900

    test SwiftUI

---
+++ SwiftUIPractice/SwiftUIPractice/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  SwiftUIPractice
+//
+//  Created by Junsu Jang on 2022/04/30.
+//
+
+import Foundation
+import UIKit
+
+class Appdelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        return true
+    }
+    
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        let sceneConfig = UISceneConfiguration(name: nil, sessionRole: connectingSceneSession.role)
+        sceneConfig.delegateClass = SceneDelegate.self
+        return sceneConfig
+    }
+}
---
+++ SwiftUIPractice/SwiftUIPractice/SceneDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.swift
+//  SwiftUIPractice
+//
+//  Created by Junsu Jang on 2022/04/30.
+//
+
+import Foundation
+import UIKit
+
+class SceneDelegate: NSObject, UIWindowSceneDelegate {
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+    }
+}
