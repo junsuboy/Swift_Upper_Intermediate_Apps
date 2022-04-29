69e3f3b00c86881e0f17b9d49aa227bdc9814599
Ik Jum Moon <86935775+junsuboy@users.noreply.github.com>
2022-04-30 07:44:36 +0900

    add Project
    to practice SwiftUI

---
+++ SwiftUIPractice/SwiftUIPractice/SwiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracticeApp.swift
+//  SwiftUIPractice
+//
+//  Created by Junsu Jang on 2022/04/30.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
