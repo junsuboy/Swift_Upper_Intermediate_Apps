77d7789da7cc0f02a05a989b6ba4448cc329081a
Ik Jum Moon <86935775+junsuboy@users.noreply.github.com>
2022-05-02 15:45:04 +0900

    TabView 생성

--- MyAssets/MyAssets/SceneDelegate.swift
+++ MyAssets/MyAssets/SceneDelegate.swift
@@ -7,9 +7,20 @@
 
 import Foundation
 import UIKit
+import SwiftUI
 
 class SceneDelegate: NSObject, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        let contentView = ContentView()
         
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UIHostingController(rootView: contentView)
+            self.window = window
+            window.makeKeyAndVisible()
+        }
     }
 }
