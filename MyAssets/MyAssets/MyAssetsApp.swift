137e461e9e1d8508092b983ee65a4daf67785868
Ik Jum Moon <86935775+junsuboy@users.noreply.github.com>
2022-05-02 15:24:47 +0900

    자산관리 앱 생성
    SwiftUI 사용 프로젝트

---
+++ MyAssets/MyAssets/MyAssetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAssetsApp.swift
+//  MyAssets
+//
+//  Created by Junsu Jang on 2022/05/02.
+//
+
+import SwiftUI
+
+@main
+struct MyAssetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyAssets/MyAssets/SceneDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.swift
+//  MyAssets
+//
+//  Created by Junsu Jang on 2022/05/02.
+//
+
+import Foundation
+import UIKit
+
+class SceneDelegate: NSObject, UIWindowSceneDelegate {
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+    }
+}
