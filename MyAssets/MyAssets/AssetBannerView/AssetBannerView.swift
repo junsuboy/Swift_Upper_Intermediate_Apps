268da26e83f449c3a7f30499f00e1fe811d11a4c
Ik Jum Moon <86935775+junsuboy@users.noreply.github.com>
2022-05-04 10:45:27 +0900

    Banner 추가 완료

--- MyAssets/MyAssets/AssetBannerView/AssetBannerView.swift
+++ MyAssets/MyAssets/AssetBannerView/AssetBannerView.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 struct AssetBannerView: View {
     let bannerList: [AssetBanner] = [
-        AssetBanner(title: "공지사항", description: "추가된 공지사항을 확인하세요", backgroundColor: .red)
-        AssetBanner(title: "주말 이벤트", description: "주말 이벤트 놓치지 마세요", backgroundColor: .yellow)
-        AssetBanner(title: "깜짝 이벤트", description: "엄청난 이벤트에 놀라지 마세요", backgroundColor: .blue)
+        AssetBanner(title: "공지사항", description: "추가된 공지사항을 확인하세요", backgroundColor: .red),
+        AssetBanner(title: "주말 이벤트", description: "주말 이벤트 놓치지 마세요", backgroundColor: .yellow),
+        AssetBanner(title: "깜짝 이벤트", description: "엄청난 이벤트에 놀라지 마세요", backgroundColor: .blue),
         AssetBanner(title: "가을 프로모션", description: "가을...🍂", backgroundColor: .brown)
     ]
     @State private var currentPage = 0
@@ -20,7 +20,10 @@ struct AssetBannerView: View {
         let bannerCards = bannerList.map { BannerCard(banner: $0) }
         
         ZStack(alignment: .bottomTrailing) {
-            PageViewController
+            PageViewController(pages: bannerCards, currentPage: $currentPage)
+            PageControl(numberOfPages: bannerList.count, currentPage: $currentPage)
+                .frame(width: CGFloat(bannerCards.count * 18))
+                .padding(.trailing)
         }
     }
 }
@@ -28,5 +31,6 @@ struct AssetBannerView: View {
 struct AssetBannerView_Previews: PreviewProvider {
     static var previews: some View {
         AssetBannerView()
+            .aspectRatio(5/2, contentMode: .fit)
     }
 }
