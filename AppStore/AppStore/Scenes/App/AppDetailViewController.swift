9d986bd13f547d3e559be548904d59ce8a7716f2
Ik Jum Moon <86935775+junsuboy@users.noreply.github.com>
2022-05-16 11:45:51 +0900

    공유 기능 구현 (UIActivityViewController)

--- AppStore/AppStore/Scenes/App/AppDetailViewController.swift
+++ AppStore/AppStore/Scenes/App/AppDetailViewController.swift
@@ -51,9 +51,12 @@ final class AppDetailViewController: UIViewController {
         let button = UIButton()
         button.setImage(UIImage(systemName: "square.and.arrow.up"), for: .normal)
         button.tintColor = .systemBlue
+        button.addTarget(self, action: #selector(didTapShareButton), for: .touchUpInside)
         
         return button
     }()
+    
+ 
     
     init(today: Today) {
         self.today = today
@@ -122,6 +125,12 @@ private extension AppDetailViewController {
             $0.trailing.equalTo(titleLabel.snp.trailing)
             $0.width.equalTo(32.0)
         }
+    }
+    
+    @objc func didTapShareButton() {
+        let activityItems: [Any] = [today.title]
+        let activityViewController = UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
+        present(activityViewController, animated: true)
     }
 }
 
