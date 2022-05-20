d7df4f701d1621d4ac600b27e2c5ece41f9fe506
Ik Jum Moon <86935775+junsuboy@users.noreply.github.com>
2022-05-20 23:28:38 +0900

    api response model 구현

---
+++ Subway/Subway/StationResponseModel.swift
@@ -0,0 +1,33 @@
+//
+//  StationResponseModel.swift
+//  Subway
+//
+//  Created by Junsu Jang on 2022/05/20.
+//
+
+import Foundation
+
+struct StationResponseModel: Decodable {
+    var stations: [Station] { searchInfo.row }
+    
+    private let searchInfo: SearchInfoBySubwayNameServiceModel
+    
+    enum CodingKeys: String, CodingKey {
+        case searchInfo = "SearchInfoBySubwayNameService"
+    }
+    
+    struct SearchInfoBySubwayNameServiceModel: Decodable {
+        var row: [Station] = []
+    }
+    
+}
+
+struct Station: Decodable {
+    let stationName: String
+    let lineNumber: String
+
+    enum CodingKeys: String, CodingKey {
+        case stationName = "STATION_NM"
+        case lineNumber = "LINE_NUM"
+    }
+}
