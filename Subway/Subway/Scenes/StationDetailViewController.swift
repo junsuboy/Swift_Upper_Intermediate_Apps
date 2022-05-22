8181df0a329616b15d14b2cd60fe401c57cf330b
Ik Jum Moon <86935775+junsuboy@users.noreply.github.com>
2022-05-22 22:47:46 +0900

    역별 정보 불러오기 구현 완료

--- Subway/Subway/Scenes/StationDetailViewController.swift
+++ Subway/Subway/Scenes/StationDetailViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Junsu Jang on 2022/05/17.
 //
 
+import Alamofire
 import SnapKit
 import UIKit
 
@@ -15,10 +16,7 @@ final class StationDetailViewController: UIViewController {
         return refreshControl
     }()
     
-    @objc func fetchData() {
-        print("REFRESH !")
-        refreshControl.endRefreshing()
-    }
+    
     
     private lazy var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
@@ -47,6 +45,24 @@ final class StationDetailViewController: UIViewController {
         
         view.addSubview(collectionView)
         collectionView.snp.makeConstraints { $0.edges.equalToSuperview() }
+        
+        fetchData()
+    }
+    
+    @objc private func fetchData() {
+        let stationName = "서울역"
+        let urlString = "http://swopenapi.seoul.go.kr/api/subway/sample/json/realtimeStationArrival/0/5/\(stationName.replacingOccurrences(of: "역", with: ""))"
+        
+        AF
+            .request(urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? "")
+            .responseDecodable(of: StationArrivalDataResponseModel.self) { [weak self] response in
+                self?.refreshControl.endRefreshing()
+                
+                guard case .success(let data) = response.result else { return }
+                
+                print(data.realtimeArrivalList)
+            }
+            .resume()
     }
 }
 
