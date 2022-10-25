89a7be83f07cac3c301c99b01aa8c99b1c1a94f3
Ik Jum Moon <86935775+junsuboy@users.noreply.github.com>
2022-10-26 00:54:20 +0900

    UI Construction Complete

---
+++ GitHubRepository/GitHubRepository/RepositoryListCell.swift
@@ -0,0 +1,32 @@
+//
+//  RepositoryListCell.swift
+//  GitHubRepository
+//
+//  Created by Junsu Jang on 2022/10/26.
+//
+
+import UIKit
+import SnapKit
+
+class RepositoryListCell: UITableViewCell {
+    var repository: String?
+    
+    let nameLabel = UILabel()
+    let descriptionLabel = UILabel()
+    let starImageView = UIImageView()
+    let starLabel = UILabel()
+    let languageLabel = UILabel()
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        [
+            nameLabel, descriptionLabel,
+            starImageView, starLabel, languageLabel
+        ].forEach {
+            contentView.addSubview($0)
+        }
+        
+        
+    }
+}
